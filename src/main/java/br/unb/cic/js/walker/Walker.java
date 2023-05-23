b7d8e70007473c899cbf595d87a0ff9c24b47e78
Rafael Campos Nunes <rafaelnunes@engineer.com>
2023-05-22 22:06:38 -0300

    Add implementation of the walker task
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/main/java/br/unb/cic/js/walker/Walker.java
+++ src/main/java/br/unb/cic/js/walker/Walker.java
@@ -1,5 +1,6 @@
 package br.unb.cic.js.walker;
 
+import br.unb.cic.js.date.Interval;
 import lombok.Builder;
 import lombok.val;
 import org.apache.logging.log4j.LogManager;
@@ -9,9 +10,13 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Vector;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 import java.util.stream.Collectors;
 
 @Builder
@@ -26,7 +31,6 @@ public class Walker {
     public final Date endDate;
 
     public void traverse() {
-
         logger.info("initializing git traversal");
         logger.info(
                 "path: {}\nproject: {}\nsteps: {}\nthreads: {}\ninitial date: {}\nend date: {}",
@@ -70,13 +74,51 @@ public class Walker {
                     return;
                 }
 
+                // create a report directory that will contain the results
+                val r = Paths.get(p.toAbsolutePath().getParent().toString(), "js-miner-out");
+                if (!r.toFile().exists()) {
+                    Files.createDirectory(r);
+                }
+
+                val pool = Executors.newFixedThreadPool(threads);
+                val tasks = new Vector<Future>();
+
                 for (Path repositoryPath: repositories) {
                     logger.info("project: {}", repositoryPath);
+
+                    val walker = RepositoryWalker.builder()
+                            .path(repositoryPath)
+                            .build();
+
+                    val report = Paths.get(r.toString(), repositoryPath.getFileName().toString() + ".csv");
+                    val interval = Interval.builder()
+                            .begin(initialDate)
+                            .end(endDate)
+                            .build();
+
+                    val task = RepositoryWalkerTask.builder()
+                            .walker(walker)
+                            .report(report)
+                            .interval(interval)
+                            .steps(steps)
+                            .build();
+
+                    tasks.add(pool.submit(task));
                 }
+
+                // wait for every task to finish
+                for (Future task : tasks) {
+                    task.get();
+                }
+
+                pool.shutdown();
             } else {
                 logger.warn("path {} does not exist or isn't a directory", p);
             }
         } catch (IOException ex) {
+            ex.printStackTrace();
+        } catch ( java.lang.InterruptedException | java.util.concurrent.ExecutionException ex) {
+            logger.error("failed to execute a concurrent task");
             ex.printStackTrace();
         }
     }
