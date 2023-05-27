04394ce9030166982fb09614faff437daac77fae
Rafael Campos Nunes <rafaelnunes@engineer.com>
2023-05-27 16:59:52 -0300

    Add visitors to other statements/declarations
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/main/java/br/unb/cic/js/miner/JSVisitor.java
+++ src/main/java/br/unb/cic/js/miner/JSVisitor.java
@@ -10,9 +10,12 @@ public class JSVisitor extends JavaScriptParserBaseVisitor<Void> {
 	int totalAsyncDeclarations = 0;
 	int totalAwaitDeclarations = 0;
 	int totalLetDeclarations = 0;
-
+	int totalConstDeclaration = 0;
 	int totalClassDeclarations = 0;
 	int totalYieldDeclarations = 0;
+	int totalExportDeclarations = 0;
+	int totalImportStatements = 0;
+	int totalRestStatements = 0;
 
 	@Override
 	public Void visitFunctionDeclaration(FunctionDeclarationContext ctx) {
@@ -50,18 +53,6 @@ public class JSVisitor extends JavaScriptParserBaseVisitor<Void> {
 	}
 
 	@Override
-	public Void visitLet_(Let_Context ctx) {
-		if (ctx.StrictLet() != null) {
-			totalLetDeclarations++;
-		}
-		if (ctx.NonStrictLet() != null) {
-			totalLetDeclarations++;
-		}
-
-		return super.visitLet_(ctx);
-	}
-
-	@Override
 	public Void visitYieldExpression(YieldExpressionContext ctx) {
 		if (ctx.yieldStatement() != null) {
 			totalYieldDeclarations++;
@@ -75,6 +66,49 @@ public class JSVisitor extends JavaScriptParserBaseVisitor<Void> {
 			totalClassDeclarations++;
 		}
 		return super.visitClassDeclaration(ctx);
+	}
+
+	@Override
+	public Void visitExportDeclaration(ExportDeclarationContext ctx) {
+		if (ctx.Export() != null) {
+			totalExportDeclarations++;
+		}
+		return super.visitExportDeclaration(ctx);
+	}
+
+	@Override
+	public Void visitImportExpression(ImportExpressionContext ctx) {
+		if (ctx.Import() != null) {
+			totalImportStatements++;
+		}
+		return super.visitImportExpression(ctx);
+	}
+
+	@Override
+	public Void visitImportStatement(ImportStatementContext ctx) {
+		if (ctx.Import() != null) {
+			totalImportStatements++;
+		}
+		return super.visitImportStatement(ctx);
+	}
+
+	@Override
+	public Void visitLastFormalParameterArg(LastFormalParameterArgContext ctx) {
+		if (ctx.Ellipsis() != null) {
+			totalRestStatements++;
+		}
+		return super.visitLastFormalParameterArg(ctx);
+	}
+
+	@Override
+	public Void visitVarModifier(VarModifierContext ctx) {
+		if (ctx.Const() != null) {
+			totalConstDeclaration++;
+		}
+		if (ctx.let_() != null) {
+			totalLetDeclarations++;
+		}
+		return super.visitVarModifier(ctx);
 	}
 
 	@Override
