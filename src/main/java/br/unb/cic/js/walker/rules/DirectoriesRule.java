06df2d6d3ab9b193867e964e260af308ea3faa28
Rafael Campos Nunes <rafaelnunes@engineer.com>
2023-09-12 11:40:04 -0300

    Add 3 more folders in the directories rule
    Signed-off-by: Rafael Campos Nunes <rafaelnunes@engineer.com>

--- src/main/java/br/unb/cic/js/walker/rules/DirectoriesRule.java
+++ src/main/java/br/unb/cic/js/walker/rules/DirectoriesRule.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
 @NoArgsConstructor
 public final class DirectoriesRule {
 
-    private static final String[] directories = {"lib", "vendor", "node_modules", "libs", "coverage", "build", "bin", "stories", "dist", "external"};
+    private static final String[] directories = {"lib", "vendor", "node_modules", "libs", "coverage", "build", "bin", "stories", "dist", "external", "3rdParty", "thirdparty", "third-party"};
 
     /**
      * This method filters out directories that match with a list of words defined within. The directories contain
